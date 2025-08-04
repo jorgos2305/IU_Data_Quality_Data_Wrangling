8a8db001abe46b75b8829a4c597fea49d8b56480
jorgos2305 <jorge.tellez.rosales@outlook.com>
2025-08-04 23:44:34 +0200

    Add API name to the data storage path
    To ensure raw data is store in the correct directory helpers.store uses
    the API name in the storage path.

--- utils/helpers.py
+++ utils/helpers.py
@@ -20,7 +20,7 @@ def load_openweather_cities() -> List[str]:
 
 def store(raw_data:dict, api_name:str) -> None:
     # get the project's root directory
-    raw_data_directory = Path(__file__).resolve().parents[1].joinpath(r"data/raw")
+    raw_data_directory = Path(__file__).resolve().parents[1].joinpath(fr"data/raw/{api_name}")
     raw_data_directory.mkdir(parents=True, exist_ok=True)
     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
     file_name = raw_data_directory.joinpath(f"{current_time}_{api_name}.json")
